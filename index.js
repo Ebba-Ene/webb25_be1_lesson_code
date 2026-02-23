5b35788d667a4810361addfbd82b709e1889a3f0
Desirée Strand <desireestrxnd@gmail.com>
2026-02-23 13:48:56 +0100

    add routes
    Co-authored-by: Manau Tunjae <manau.tunjae@hotmail.com>
    Co-authored-by: Henry Bouchard <bouchardhenry@gmail.com>
    Co-authored-by: Samuel <Samuel6693@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from 'dotenv';
 import cors from "cors"
+import router from "./routes/artists.js"
 
 dotenv.config();
 
@@ -9,62 +10,15 @@ const app = express();
 app.use(cors())
 app.use(express.json())
 
-const PORT = process.env.PORT || 3000
-
-let artists = [
-  { id: 1, name: 'Bad Bunny' },
-  { id: 2, name: 'Zara Larsson' },
-  { id: 3, name: 'Radiohead' },
-];
-
 app.get("/", (req, res) => {
     return res.json({
         message: "Healthy?"
     })
 })
 
-app.get("/api/artists", (req, res) => {
-  const { q } = req.query 
-  if(q) {
-    return res.json(artists.filter(artist => artist.name.includes(q)))
-  }
-  return res.json(artists)
-})
+app.use("/api/artists", router)
 
-app.get("/api/artists/:id", (req, res) => {
-  const id = Number(req.params.id)
-  if(isNaN(id)) {
-    return res.status(400).json({
-      message: "Id has to be a valid number"
-    })
-  }
-  const artist = artists.find(artist => artist.id === id)
-  if(!artist) {
-    return res.status(404).json({
-      message: "Artist does not exist"
-    })
-  }
-  return res.json(artist)
-})
-
-app.post("/api/artists", (req, res) => {
-    const { name } = req.body
-    if(!name || typeof name !== "string"){
-      return res.status(400).json({
-        message: "Name is required"
-      })
-    }
-    console.log(artists.map(a => a.id))
-    const lastId = Math.max(...artists.map(a => a.id))
-    console.log(lastId)
-    const artist = {
-      name,
-      id: lastId + 1
-    }
-
-    artists.push(artist)
-    return res.status(201).json(artist)
-})
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT,(error) => {
     if(error) {
