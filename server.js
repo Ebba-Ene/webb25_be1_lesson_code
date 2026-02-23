5427eec47e9fc5b693468681af3ca0a5f5234ba2
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2026-02-23 10:13:21 +0100

    add basic express code

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
-console.log("Hello world!")
+import express from "express"
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000
+
+app.get("/", (req, res) => {
+    console.log("Healthy")
+    return res.json({
+        message: "Healthy"
+    })
+})
+
+app.listen(PORT,(error) => {
+    if(error) {
+        console.log("Error in running express", error.message)
+        return
+    }
+    console.log(`Server is running on port ${PORT}`)
+})
