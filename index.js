f52d0f7ea2bccc4647ba0008677e9b394ccd87c6
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2026-02-23 10:44:06 +0100

    add middlewares to express

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 import express from "express"
 import dotenv from 'dotenv';
+import cors from "cors"
 
 dotenv.config();
 
 const app = express();
 
+app.use(cors())
+app.use(express.json())
+
 const PORT = process.env.PORT || 3000
 
 app.get("/", (req, res) => {
     return res.json({
-        message: "Healthy"
+        message: "Healthy?"
     })
 })
 
