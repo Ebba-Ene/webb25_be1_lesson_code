e870d48c109c1c12701e877612e8c540d1a3ab99
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2026-03-09 09:18:22 +0100

    refactor create song to handle populate

--- db/songs.js
+++ db/songs.js
@@ -28,8 +28,9 @@ export async function getSongByid(id) {
 
 export async function createSong(data) {
   try {
-    const newSong = (await Song.create(data)).populate("artist").populate("album", "title");
-    return newSong
+    const newSong = (await Song.create(data))
+    const fetchedSong = await Song.findById(newSong._id).populate("artist").populate("album", "title");
+    return fetchedSong
   } catch (err) {
     console.error("Unable to create 'Song'", err)
     return null
