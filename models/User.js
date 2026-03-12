0d84b50f7c3516c43524554b71d9e76de33ed113
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2026-03-12 10:12:35 +0100

    add basic user model and register

---
+++ models/User.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        validate: {
+            validator: function(v) {
+                return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v);
+            },
+            message: props => `${props.value} is not a valid email address!`
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 6,
+        select: false
+    }
+}, {
+    timestamps: true
+})
+
+const User = mongoose.model("User", userSchema)
+
+export default User
